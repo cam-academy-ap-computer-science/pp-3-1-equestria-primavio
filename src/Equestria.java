7467bdc65763af2efcd08ea626d3abc9c046a07e
primavio000 <primavio000@CAM-LAB-15.bgsd.k12.wa.us>
2019-10-25 07:55:30 -0700

    im not sure what to do butil work on it

--- src/Equestria.java
+++ src/Equestria.java
@@ -1,9 +1,20 @@
 
 public class Equestria {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main (String[] args) {
+		System.out.println("Distance from ")
+		
+		
+		// sqrt((x2 - x1)sq + (y2 - y1)sq)//
+				
 	}
 
+	public static double distance (double x1, double y1) {
+		// step1 subtract x2-x1, y2-y1
+		double step1_x = x2 - x1;
+		double step1_y = y2 - y1;
+		// step2 sq
+		// step3 add
+		double d = Math.sqrt(Math.pow(step1_x, 2) + Math.pow(step1_y, 2));
+	}
 }
