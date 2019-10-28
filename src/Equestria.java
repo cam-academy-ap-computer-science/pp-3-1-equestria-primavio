c9bb085aefab996695f41246b44aa7286c6186bb
Violetta <Violetta@192.168.0.7>
2019-10-28 05:10:48 -0700

    well its 5 am and what can i say:)

--- src/Equestria.java
+++ src/Equestria.java
@@ -2,19 +2,17 @@
 public class Equestria {
 
 	public static void main (String[] args) {
-		System.out.println("Distance from ")
-		
-		
-		// sqrt((x2 - x1)sq + (y2 - y1)sq)//
+		System.out.println("Distance from Baltimare to Manehattan = " + distance (4, -6));
+		System.out.println("Distance from Los Pegasus to Neighagra Falls = " + distance (18, -13));
+		System.out.println("Distance from Badlands to Ponyville = " + distance (-12, -9));
 				
 	}
-
-	public static double distance (double x1, double y1) {
-		// step1 subtract x2-x1, y2-y1
-		double step1_x = x2 - x1;
-		double step1_y = y2 - y1;
-		// step2 sq
-		// step3 add
-		double d = Math.sqrt(Math.pow(step1_x, 2) + Math.pow(step1_y, 2);
+	// sqrt((x2 - x1)sq + (y2 - y1)sq)//
+	public static double distance (int step1_x, int step1_y) {
+		// step1 subtract x2-x1, y2-y1 and put them into equation
+		// step2 add
+		// step3 sq
+		double d = Math.sqrt(Math.pow(step1_x, 2) + Math.pow(step1_y, 2));
+		return d;
 	}
 }
